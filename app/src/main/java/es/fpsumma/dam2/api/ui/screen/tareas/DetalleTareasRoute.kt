a8a88a07547bda038f74bde5e69c79251983657d
fernandotocas.23 <fernando141255@hotmail.com>
2026-01-17 17:21:00 +0100

    Actualizado, olvidé agregar el tipo de dato INT al id, por ese motivo no me llamaba el ID del Content, una vez echo, el código compiló.

--- app/src/main/java/es/fpsumma/dam2/api/ui/screen/tareas/DetalleTareasRoute.kt
+++ app/src/main/java/es/fpsumma/dam2/api/ui/screen/tareas/DetalleTareasRoute.kt
@@ -1,27 +1,21 @@
 package es.fpsumma.dam2.api.ui.screen.tareas
 
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.collectAsState
-import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.navigation.NavController
-import es.fpsumma.dam2.api.ui.navegation.Routes
-import es.fpsumma.dam2.api.ui.navegation.Routes.tareaAPIView
 import es.fpsumma.dam2.api.viewmodel.TareasViewModel
 
 @Composable
 fun DetalleTareasRoute (
+    id: Int,
     navController: NavController,
     vm: TareasViewModel,
     modifier: Modifier = Modifier,
 ){
 
-    val state by vm.state.collectAsState()
-
-    val Tarea = backStage.arguments?.getInt("id") ?: 0,
 
     DetalleTareaContent(
-        id = Tarea,
+        id = id,
         navController = navController,
         vm = vm,
         modifier = modifier,
